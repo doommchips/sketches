112192d1970f8bdafc67a548a38adbaea4395111
doommchips <dgtl.bw@gmail.com>
2017-07-17 23:31:22 +0200

    fist commit with files for rain and ash sketches

---
+++ ash/script.js
@@ -0,0 +1,18 @@
+var canvas, canvasContext;
+
+window.onload = function() {
+    canvas = document.getElementById('canvas');
+    canvasContext = canvas.getContext('2d');
+
+    canvas.width = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+
+    console.log(canvas.width + 'w ' + canvas.height + 'h');
+
+    var framesPerSecond = 30;
+    setInterval(drawAll, 1000/framesPerSecond);
+}
+
+function drawAll() {
+
+}
---
+++ ash/script2.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    var canvas = $('#canvas')[0];
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    if(canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+        var w = canvas.width + 0.2 * canvas.height;
+        var h = canvas.height;
+        ctx.strokeStyle = 'rgba(225, 224, 232, 0.7)';
+        ctx.lineWidth = 1;
+        ctx.lineCap = 'round';
+
+
+        var init = [];
+        var maxParts = 500;
+        for(var a = 0; a < maxParts; a++) {
+            init.push({
+                x: Math.random() * w,
+                y: Math.random() * h,
+                l: Math.random() * 0.8,
+                xs: Math.random() * 1 - 4,
+                ys: Math.random() * 10 + 20
+            })
+        }
+
+        var particles = [];
+        for(var b = 0; b < maxParts; b++) {
+            particles[b] = init[b];
+        }
+
+        function draw() {
+            ctx.clearRect(0, 0, w, h);
+            for(var c = 0; c < particles.length; c++) {
+                var p = particles[c];
+                ctx.beginPath();
+                ctx.moveTo(p.x, p.y);
+                ctx.lineTo(p.x + p.l * p.xs, p.y + p.l * p.ys);
+                ctx.stroke();
+            }
+            move();
+        }
+
+        function move() {
+            for(var b = 0; b < particles.length; b++) {
+                var p = particles[b];
+                p.x += p.xs;
+                p.y += p.ys;
+                if(p.x > w || p.y > h) {
+                    p.x = Math.random() * w;
+                    p.y = -20;
+                }
+            }
+        }
+
+        setInterval(draw, 30);
+
+    }
+});
