354f6be469e9dd1f220140be28021691d2208a60
doommchips <dgtl.bw@gmail.com>
2018-06-09 22:48:02 +0200

    add sync work

---
+++ sync/script.js
@@ -0,0 +1,9 @@
+// function getRandomInt(max) {
+//     return Math.floor(Math.random() * Math.floor(max));
+// }
+//
+// function drawBox() {
+//     document.body.innerHTML += '<div class="box"></div>';
+// }
+//
+// document.onload = setInterval(() => drawBox(), getRandomInt(5000));
