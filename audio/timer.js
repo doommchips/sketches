bfd7cc6e21729db83a477d0982726fed7a86fc05
doommchips <dgtl.bw@gmail.com>
2021-04-10 02:13:47 +0200

    forgot to include timer.js

---
+++ audio/timer.js
@@ -0,0 +1,16 @@
+let start = Date.now();
+let time = 0;
+let countMillisecond = document.getElementById("msec");
+let countSecond = document.getElementById("secs");
+let countMinute = document.getElementById("mins");
+let countHour = document.getElementById("hour");
+
+window.setInterval((function(){
+    return function() {
+        time = (Date.now()-start)/10;
+        countMillisecond.innerText = Math.floor(time).toLocaleString(undefined, {minimumIntegerDigits: 2});
+        // countSecond.innerText = Math.floor(time / 100).toLocaleString(undefined, {minimumIntegerDigits: 2});
+        // countMinute.innerText = Math.floor(time / 6000).toLocaleString(undefined, {minimumIntegerDigits: 2});
+        // countHour.innerText = Math.floor(time / 60000).toLocaleString(undefined, {minimumIntegerDigits: 2});
+        };
+}()), 10);
